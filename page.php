737d3533ffec3db5473394b451477a939194cadb
Ben Ashman <ben.ashman@me.com>
2013-09-22 11:52:28 +0100

    Wrap page content

--- page.php
+++ page.php
@@ -16,10 +16,12 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
-<?php comments_template( '', true ); ?>
-<?php endwhile; ?>
+<main id="main" class="main" role="main">
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+    <?php comments_template( '', true ); ?>
+    <?php endwhile; ?>
+</main>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
