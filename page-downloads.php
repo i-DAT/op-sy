c191a798300f5740bc97667c11908472e8a4860d
Ben Ashman <ben.ashman@me.com>
2013-09-22 17:34:55 +0100

    Stub out downloads page template

--- page-downloads.php
+++ page-downloads.php
@@ -5,7 +5,183 @@
         <h2><?php the_title(); ?></h2>
     </header>
     
-    Custom <?php the_title(); ?> template here, yo!
+    <section class="downloads-list">
+
+        <section class="downloads-section readers">
+
+            <h3>Readers</h3>
+
+            <div class="download github">
+                <header>
+                    <h3 class="title">ActionScript 2 Arch-OS Parser</h3>
+                    <h4 class="author">Chris Saunders</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+            <div class="download github">
+                <header>
+                    <h3 class="title">ActionScript 3 Arch-OS Parser</h3>
+                    <h4 class="author">Chris Saunders</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+            <div class="download github">
+                <header>
+                    <h3 class="title">Quartz Composer Arch-OS Parser</h3>
+                    <h4 class="author">Aqeel Akbar</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+            <div class="download github">
+                <header>
+                    <h3 class="title">PHP Arch-OS Parser</h3>
+                    <h4 class="author">Chris Saunders</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+            <div class="download github">
+                <header>
+                    <h3 class="title">Processing Arch-OS Parser</h3>
+                    <h4 class="author">Chris Saunders</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+            <div class="download github">
+                <header>
+                    <h3 class="title">VVVV</h3>
+                    <h4 class="author">Darrel Fox</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+            <div class="download github">
+                <header>
+                    <h3 class="title">Blender Arch-OS Reader</h3>
+                    <h4 class="author">Pete Carss</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+        </section>
+
+        <section class="downloads-section ecoid">
+
+            <h3>Ecoid Readers</h3>
+
+            <div class="download github">
+                <header>
+                    <h3 class="title">ActionScript 2 Arch-OS Parser</h3>
+                    <h4 class="author">Chris Saunders</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+            <div class="download github">
+                <header>
+                    <h3 class="title">ActionScript 3 Arch-OS Parser</h3>
+                    <h4 class="author">Chris Saunders</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+            <div class="download github">
+                <header>
+                    <h3 class="title">Quartz Composer Arch-OS Parser</h3>
+                    <h4 class="author">Aqeel Akbar</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+        </section>
+
+        <section class="downloads-section ecoid">
+
+            <h3>Recommended software</h3>
+
+            <div class="download">
+                <header>
+                    <h3 class="title">ActionScript 2 Arch-OS Parser</h3>
+                    <h4 class="author">Chris Saunders</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+            <div class="download">
+                <header>
+                    <h3 class="title">ActionScript 3 Arch-OS Parser</h3>
+                    <h4 class="author">Chris Saunders</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+            <div class="download">
+                <header>
+                    <h3 class="title">Quartz Composer Arch-OS Parser</h3>
+                    <h4 class="author">Aqeel Akbar</h4>
+                </header>
+
+                <ul class="links">
+                    <li><a href="#">Download (.zip)</a></li>
+                    <li><a href="#">View on GitHub</a></li>
+                </ul>
+            </div>
+
+        </section>
+
+    </section>
 </main>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
