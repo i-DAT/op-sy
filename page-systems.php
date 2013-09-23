d274ed82536dcea6e72c7517a9be8110ecc09ef0
Ben Ashman <ben.ashman@me.com>
2013-09-23 23:20:21 +0100

    Stub out systems page layout

--- page-systems.php
+++ page-systems.php
@@ -5,7 +5,29 @@
         <h2><?php the_title(); ?></h2>
     </header>
     
-    Custom <?php the_title(); ?> template here, yo!
+    <section class="systems-list">
+        <div class="system">
+            <header class="system-header">
+                <h3>Arch-OS</h3>
+                <img src="http://placekitten.com/1050/400"/>
+            </header>
+            <div class="description">
+                <p>Arch-OS represents an evolution in intelligent architecture, interactive art and ubiquitous computing. An ‘Operating System’ for contemporary architecture (Arch-OS, ’software for buildings’) has been developed to manifest the life of a building and provide artists, engineers and scientists with a unique environment for developing transdisciplinary work and new public art.</p>
+                <p>The Arch-OS experience combines a rich mix of the physical and virtual by incorporating the technology of ’smart’ buildings into new dynamic virtual architectures.</p>
+            </div>
+        </div>
+
+        <div class="system">
+            <header class="system-header">
+                <h3>i-500</h3>
+                <img src="http://placekitten.com/1050/400"/>
+            </header>
+            <div class="description">
+                <p>Arch-OS represents an evolution in intelligent architecture, interactive art and ubiquitous computing. An ‘Operating System’ for contemporary architecture (Arch-OS, ’software for buildings’) has been developed to manifest the life of a building and provide artists, engineers and scientists with a unique environment for developing transdisciplinary work and new public art.</p>
+                <p>The Arch-OS experience combines a rich mix of the physical and virtual by incorporating the technology of ’smart’ buildings into new dynamic virtual architectures.</p>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
