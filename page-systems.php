c2c51d30bec996a20680bee5af081c7dbcba674f
Ben Ashman <ben.ashman@me.com>
2013-09-24 00:01:11 +0100

    Stub out information page layout

--- page-systems.php
+++ page-systems.php
@@ -4,7 +4,11 @@
     <header class="page-header">
         <h2><?php the_title(); ?></h2>
     </header>
-    
+
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+
     <section class="systems-list">
         <div class="system">
             <header class="system-header">
--- page.php
+++ page.php
@@ -17,9 +17,6 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <main id="main" class="main" role="main">
-    <?php // if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-    <?php // the_content(); ?>
-    <?php // endwhile; ?>
 
     <section class="about">
         <section class="intro">
