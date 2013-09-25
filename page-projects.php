dcdb267ceb92b6e5cb917bc98433869d042dd2ce
Ben Ashman <ben.ashman@me.com>
2013-09-25 20:25:37 +0100

    Responsivize header and retain aspect ratio on images

--- page-projects.php
+++ page-projects.php
@@ -6,9 +6,31 @@
     </header>
     
     <section class="projects-list">
-       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-           <?php the_content(); ?>
-       <?php endwhile; ?>
+         <div class="project">
+            <header class="project-header">
+                <h3><a href="#">Arch-OS</a></h3>
+                <img src="http://placekitten.com/1050/400"/>
+            </header>
+
+            <div class="description">
+                <p>Arch-OS represents an evolution in intelligent architecture, interactive art and ubiquitous computing. An ‘Operating System’ for contemporary architecture (Arch-OS, ’software for buildings’) has been developed to manifest the life of a building and provide artists, engineers and scientists with a unique environment for developing transdisciplinary work and new public art.</p>
+                <p>The Arch-OS experience combines a rich mix of the physical and virtual by incorporating the technology of ’smart’ buildings into new dynamic virtual architectures.</p>
+                <a href="#">View project</a>
+            </div>
+        </div>
+
+         <div class="project">
+            <header class="project-header">
+                <h3><a href="#">Arch-OS</a></h3>
+                <img src="http://placekitten.com/1050/400"/>
+            </header>
+
+            <div class="description">
+                <p>Arch-OS represents an evolution in intelligent architecture, interactive art and ubiquitous computing. An ‘Operating System’ for contemporary architecture (Arch-OS, ’software for buildings’) has been developed to manifest the life of a building and provide artists, engineers and scientists with a unique environment for developing transdisciplinary work and new public art.</p>
+                <p>The Arch-OS experience combines a rich mix of the physical and virtual by incorporating the technology of ’smart’ buildings into new dynamic virtual architectures.</p>
+                <a href="#">View project</a>
+            </div>
+        </div>
     </section>
 </main>
 
