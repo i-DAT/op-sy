d3d71c92e18838f83f85e01a93bb59c7b0d97ad1
Saul Hardman <hello@iamsaul.co.uk>
2013-09-28 16:46:45 +0100

    Add google analytics, update README, improve compression
    - use grunt-contrib-compression for zipping

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -1,8 +1,10 @@
     
     <?php wp_footer(); ?>
     <?php if (WP_DEBUG): ?>
+    <script>var config = { 'env': 'development' };</script>
     <script src="<?php echo get_template_directory_uri(); ?>/components/requirejs/require.js" data-main="<?php echo get_template_directory_uri(); ?>/js/main"></script>
     <?php else: ?>
+    <script>var config = { 'env': 'production' };</script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/main.min.js"></script>
     <?php endif; ?>
 	</body>
